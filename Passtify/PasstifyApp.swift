2ce71ef3cc92225f0c1ff554f65735ea95866b81
long.phan <long.phan@onemount.com>
2025-04-29 15:46:45 +0700

    init empty project

---
+++ Passtify/PasstifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasstifyApp.swift
+//  Passtify
+//
+//  Created by LONGPHAN on 29/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PasstifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
